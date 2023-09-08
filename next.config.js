832e4f4164b21b3151b686b39b8318ebf999c7dc
Steven Lucas <steven.lucas2201@gmail.com>
2023-09-08 14:21:12 -0500

    next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    formats: ["image/avif", "image/webp"],
+    formats: ["image/avif", "image/webp", "image/png", "image/jpeg", "image/jpg"],
     domains: ["drive.google.com", "mega.nz"],
   },
 };
