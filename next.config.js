8af3f9ff3ca73a21e98f7ca860a86a7d4c6ec4c9
Steven Lucas <steven.lucas2201@gmail.com>
2023-09-08 15:40:32 -0500

    new nextconfig for getServerSideProps

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
     domains: ["drive.google.com", "mega.nz"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/(.*)",
+        destination: "/",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
