644b3729c9e8ac9a47b90a74c05388c331894cc3
Steven Lucas <steven.lucas2201@gmail.com>
2023-09-29 11:09:22 -0500

    fixed bug

--- pages/shop/[id].jsx
+++ pages/shop/[id].jsx
@@ -1,3 +1,11 @@
+import React from "react";
+
+const ShopProduct = () => {
+  return <div>ShopProduct</div>;
+};
+
+export default ShopProduct;
+
 export async function getServerSideProps(context) {
   return {
     props: {}, // will be passed to the page component as props
