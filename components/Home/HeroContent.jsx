76680434d62fb79352b636727d445c15ae4c8ed4
Steven Lucas <steven.lucas2201@gmail.com>
2023-11-08 12:47:45 +0100

    homeContent

--- components/Home/HeroContent.jsx
+++ components/Home/HeroContent.jsx
@@ -1,30 +1,70 @@
-import { LanguageContext } from "@/contexts/LanguageContext";
-import { renderTextWithLineBreaks } from "@/lib/renderTextWithLineBreaks";
-import { translate } from "@/lib/translations/translate";
-import { Grid, Typography } from "@mui/material";
-import React, { useContext } from "react";
+import { LanguageContext } from '@/contexts/LanguageContext';
+import { renderTextWithLineBreaks } from '@/lib/renderTextWithLineBreaks';
+import { translate } from '@/lib/translations/translate';
+import { Add, Info, Remove } from '@mui/icons-material';
+import { Button, Collapse, Grid, Typography } from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { useContext, useState } from 'react';
 
 const HeroContent = () => {
+  const [isShown, setIsShown] = useState(false);
   const { language } = useContext(LanguageContext);
 
   return (
-    <Grid container sx={{ maxWidth: "1050px", width: "100%", margin: "1.2rem auto", justifyContent: "center", textAlign: "center" }}>
+    <Grid
+      container
+      sx={{ maxWidth: '1050px', width: '100%', margin: '1.2rem auto', justifyContent: 'center', textAlign: 'center' }}
+    >
       <Grid item xs={12}>
-        <Typography variant='h2' mb={2}>
-          Wink Monaco
-        </Typography>
+        <Image
+          width={216.5}
+          height={72}
+          src={'/icons/fullLogo.png'}
+          alt='Wink Logo'
+          layout='responsive'
+          style={{ maxWidth: '216.5px' }}
+        />
       </Grid>
-      <Grid item xs={12} md={6} sx={{ paddingRight: { xs: 0, md: 4 } }}>
-        <Typography>{renderTextWithLineBreaks(translate({ tKey: "home.heroText", lang: language }))}</Typography>
+      <Grid item xs={12} sx={{ padding: 'auto 2rem', marginTop: '1.5rem' }}>
+        <Typography>{renderTextWithLineBreaks(translate({ tKey: 'home.heroText', lang: language }))}</Typography>
       </Grid>
-      <Grid item xs={12} md={6} sx={{ paddingLeft: { xs: 0, md: 4 }, height: { xs: "250px", sm: "350px", md: "auto" }, marginTop: { xs: 4, md: 0 } }}>
+      <Collapse in={isShown}>
+        <Grid item xs={12} sx={{ padding: 'auto 2rem' }}>
+          <Typography mt={4}>
+            {renderTextWithLineBreaks(translate({ tKey: 'home.heroText2', lang: language }))}
+          </Typography>
+          <Link href='/about'>
+            <Button variant='contained' color='secondary' startIcon={<Info />} sx={{ mt: 2, mb: 2 }}>
+              {translate({ tKey: 'nav.about', lang: language })}
+            </Button>
+          </Link>
+        </Grid>
+      </Collapse>
+      <Button onClick={() => setIsShown(!isShown)} startIcon={isShown ? <Remove /> : <Add />}>
+        {isShown
+          ? translate({ tKey: 'general.reduce', lang: language })
+          : translate({ tKey: 'general.readMore', lang: language })}
+      </Button>
+      <Grid
+        item
+        xs={12}
+        sx={{
+          paddingLeft: 2,
+          paddingRight: 2,
+          height: 'auto',
+          minHeight: '400px',
+          marginTop: 4,
+        }}
+      >
         <iframe
           style={{
-            borderRadius: "1rem",
-            border: "none",
-            boxShadow: "2px 2px 10px -2px rgba(0,0,0,0.75)",
-            WebkitBoxShadow: "2px 2px 10px -2px rgba(0,0,0,0.75)",
-            MozBoxShadow: "2px 2px 10px -2px rgba(0,0,0,0.75)",
+            borderRadius: '1rem',
+            border: 'none',
+            boxShadow: '2px 2px 10px -2px rgba(0,0,0,0.75)',
+            WebkitBoxShadow: '2px 2px 10px -2px rgba(0,0,0,0.75)',
+            MozBoxShadow: '2px 2px 10px -2px rgba(0,0,0,0.75)',
           }}
           width='100%'
           height='100%'
--- components/Home/LatestNews.jsx
+++ components/Home/LatestNews.jsx
@@ -1,14 +1,14 @@
-import { translate } from "@/lib/translations/translate";
-import { Grid, Typography, useMediaQuery } from "@mui/material";
-import React from "react";
-import { ArticleCard } from "../articles/ArticleCard";
-import { useRouter } from "next/router";
-import Link from "next/link";
-import { Newspaper } from "@mui/icons-material";
+import { translate } from '@/lib/translations/translate';
+import { Grid, Typography, useMediaQuery } from '@mui/material';
+import React from 'react';
+import { ArticleCard } from '../articles/ArticleCard';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { Newspaper } from '@mui/icons-material';
 
 const LatestNews = ({ articles, language }) => {
   const router = useRouter();
-  const isMobile = useMediaQuery("(max-width:600px)"); // Check if the screen width is less than or equal to 600px
+  const isMobile = useMediaQuery('(max-width:600px)'); // Check if the screen width is less than or equal to 600px
 
   const handleClick = (id) => {
     router.push(`/articles/${id}`);
@@ -21,15 +21,16 @@ const LatestNews = ({ articles, language }) => {
     <Grid
       container
       sx={{
-        maxWidth: "1050px",
-        width: "100%",
-        margin: "1.2rem auto",
-        justifyContent: "center",
-        textAlign: "center",
-      }}>
+        maxWidth: '1050px',
+        width: '100%',
+        margin: '1.2rem auto',
+        justifyContent: 'center',
+        textAlign: 'center',
+      }}
+    >
       <Grid item xs={12}>
         <Typography variant='h4' mt={4} mb={2}>
-          {translate({ tKey: "home.latestNews", lang: language })}
+          {translate({ tKey: 'home.latestNews', lang: language })}
         </Typography>
       </Grid>
       {mobileArticles.map((article, i) => (
@@ -43,24 +44,25 @@ const LatestNews = ({ articles, language }) => {
         md={6}
         mt={2}
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-        }}>
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
         <Newspaper
           sx={{
-            fontSize: "2rem",
-            color: "primary.main",
-            marginBottom: "0.5rem",
-            display: { xs: "none", md: articles.length > 1 ? "none" : "flex" },
+            fontSize: '2rem',
+            color: 'primary.main',
+            marginBottom: '0.5rem',
+            display: { xs: 'none', md: articles.length > 1 ? 'none' : 'flex' },
           }}
         />
-        <Typography sx={{ padding: { xs: "0", md: "0 5rem" } }}>
-          {translate({ tKey: "home.latestNewsText", lang: language })}
-          <Link style={{ textDecoration: "none", color: "#22c6fe", fontWeight: 600 }} href='/articles'>
-            {translate({ tKey: "nav.articles", lang: language })}
-          </Link>{" "}
+        <Typography sx={{ padding: { xs: '0', md: '0 5rem' } }}>
+          {translate({ tKey: 'home.latestNewsText', lang: language })}
+          <Link style={{ textDecoration: 'none', color: '#22c6fe', fontWeight: 600 }} href='/articles'>
+            {translate({ tKey: 'nav.articles', lang: language })}
+          </Link>
           !
         </Typography>
       </Grid>
